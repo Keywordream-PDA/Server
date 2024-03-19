24313950c58e4f95b2f7da8d764fac5f4d6bb081
김영석 <kimyoungseok15@gmail.com>
2024-03-19 09:13:28 +0900

    feat: 신한api 연결 완료

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,7 @@ const { json } = require('body-parser');
 const express = require('express')
 const router = express.Router()
 const trendAPI = require('google-trends-api')
+const { fetchMostExchanged, fetchMostIncreased, fetchMostViewed } = require('../utils/Shinhan')
 
 router.get('/hot', function(req, res, next){
     // 한국 시간 기준으로 현재 날짜 계산
@@ -9,7 +10,6 @@ router.get('/hot', function(req, res, next){
     const krTimezoneOffset = 9 * 60; // 한국 시간대는 UTC+9
     const krNow = new Date(now.getTime() + krTimezoneOffset * 60 * 1000);
     const krDateString = krNow.toISOString().slice(0, 10);
-
     trendAPI.dailyTrends({
     trendDate: new Date(krDateString),
     geo: 'KR',
@@ -44,5 +44,39 @@ router.get('/hot', function(req, res, next){
     }
     });
 })
+let mostExchanged = [];
+let mostIncreased = [];
+let mostViewed = [];
+
+const fetchData = async () => {
+    try {
+      mostExchangedRes = await fetchMostExchanged();
+      mostIncreasedRes = await fetchMostIncreased();
+      mostViewedRes = await fetchMostViewed();
+      mostExchanged = mostExchangedRes.dataBody
+      mostIncreased = mostIncreasedRes.dataBody
+      mostViewed = mostViewedRes.dataBody
+      console.log('Data updated successfully.');
+    } catch (error) {
+      console.error('Error updating data:', error);
+    }
+  };
+
+// 초기 데이터 로드
+fetchData();
+// 주기적으로 데이터 갱신
+setInterval(fetchData, 60000); // 1분마다 갱신
+
+router.get('/most-exchanged', (req, res) => {
+  res.json(mostExchanged);
+});
+
+router.get('/most-increased', (req, res) => {
+  res.json(mostIncreased);
+});
+
+router.get('/most-viewed', (req, res) => {
+  res.json(mostViewed);
+});
 
 module.exports = router
