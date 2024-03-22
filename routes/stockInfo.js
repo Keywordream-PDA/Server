61857b96935e0f4ffb67296df939e6ce681c484f
김영석 <kimyoungseok15@gmail.com>
2024-03-22 10:23:54 +0900

    feat: 디테일 페이지 상단 구현 완료

--- routes/stockInfo.js
+++ routes/stockInfo.js
@@ -1,15 +1,25 @@
 var express = require("express");
 var router = express.Router();
 const { getStockInfo } = require("../utils/StockInfo");
+const { getAccessToken } = require("../utils/token/KOInvToken")
+// 서버 실행시 access token 발급
+let accessToken = '';
+
+const getToken = async () => {
+  try {
+    // accessToken = await getAccessToken();
+  } catch {
+
+  }
+};
+getToken();
 
 
 /* GET : keyword 가져오기 */
 router.get('/:stockCode', async function (req, res, next) {
-    console.log('들어옴')
     const stockCode = req.params.stockCode;
-    getStockInfo(stockCode).then((rows) => {
-        const result = rows[0].name;
-        res.json(result);
+    getStockInfo(stockCode, accessToken).then((infoObj) => {
+        res.json(infoObj);
     }).catch((err) => {
         console.log("DB Connection Failed:", err);
     });
--- utils/StockInfo.js
+++ utils/StockInfo.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const { pool } = require("../database/connect/mariadb.js");
+require('dotenv').config()
 
 // 주식 종목 리스트 DB에 저장
 async function getStockName(stockCode) {
@@ -8,7 +9,7 @@ async function getStockName(stockCode) {
     conn = await pool.getConnection();
     rows = await conn.query("SELECT * from Stock WHERE stockCode = ?;", stockCode);
     conn.release();
-    return rows;
+    return rows[0]
   } catch (error) {
     throw error;
   } finally {
@@ -16,18 +17,42 @@ async function getStockName(stockCode) {
   }
 }
 
-// main
-async function getStockInfo(stockCode) {
+async function getStockPrice(stockCode, accessToken){
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: `https://openapi.koreainvestment.com:9443/uapi/domestic-stock/v1/quotations/inquire-price?FID_COND_MRKT_DIV_CODE=J&FID_INPUT_ISCD=${stockCode}`,
+    headers: { 
+      'content-type': 'application/json; charset=utf-8', 
+      // 'authorization': `Bearer ${accessToken}`, 
+      // 일단 하드코딩 - 22일 19:39:43 만료
+      'authorization' : 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6IjU3YzcyYmQxLWRjMmQtNDUwNi05ZjRlLWQ3YzJiN2Y3NGRjYyIsImlzcyI6InVub2d3IiwiZXhwIjoxNzExMTAzOTgzLCJpYXQiOjE3MTEwMTc1ODMsImp0aSI6IlBTem8weFJOQVhFNlh5QTVPSmRrbVNKSVl3dVZVR2dTSGcybCJ9.DktA9eEUsmBzrW-KJ19L2jqWM1ndQ_XX08X5627TB2LjiImy68TYfMJGgV-whtGSK5x_o0lgXUiQZwHuHDj46w',
+      'appkey': process.env.KO_INV_APP_KEY, 
+      'appsecret': process.env.KO_INV_APP_SECRET,
+      'tr_id': 'FHKST01010100'
+    }
+  };
+
   try {
-    // 이 줄에서 한투에 api를 쏘는 함수 실행
-    const stockInfo = await getStockName(stockCode);
-    return stockInfo
+    const response = await axios.request(config);
+    const output = response.data.output;
+    const res = { price: output.stck_prpr, ratio: output.prdy_ctrt };
+    return res;
   } catch (error) {
-    console.error("Error in getStockInfo:", error);
-    throw error
+    console.log(error);
+    throw error; // 오류를 다시 throw하여 호출 쪽에서 처리할 수 있도록 합니다.
   }
 }
 
-// getStockInfo();
+// main
+async function getStockInfo(stockCode, accessToken) {
+  try {
+    const [stockName, stockPrice] = await Promise.all([getStockName(stockCode), getStockPrice(stockCode, accessToken)])
+    return {stockName: stockName, stockPrice: stockPrice}
+  } catch (error) {
+    console.error("Error in getStockInfo:", error); 
+    throw error
+  }
+}
 
 module.exports = { getStockName, getStockInfo };
