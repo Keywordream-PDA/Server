43b02b5e9d37d3a6fc9680a86d13333ec1ef1ecd
eehanseul <gkstmf616@gmail.com>
2024-03-28 10:36:40 +0900

    fix: select order by 삭제

--- routes/stockInfo.js
+++ routes/stockInfo.js
@@ -8,7 +8,7 @@ let accessToken = "";
 const getToken = async () => {
   try {
     // accessToken = await getAccessToken();
-    accessToken = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6IjBhMDEwZmEwLWMzMDEtNDRkNS1iYThhLWMxZDc3OTZiYmM0NyIsImlzcyI6InVub2d3IiwiZXhwIjoxNzExNTg0ODEzLCJpYXQiOjE3MTE0OTg0MTMsImp0aSI6IlBTem8weFJOQVhFNlh5QTVPSmRrbVNKSVl3dVZVR2dTSGcybCJ9.efmlzGYdYHpAfaAo_YJfJIwx6DEaJdb-p_udCFclpi-3Z7-mdSp98z_b_JpMUpoYV4jzoTHGUv3jncO5sLGeSw'
+    accessToken = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6ImVkZWEzNDE2LWYwYWQtNDY4MC04ODgwLTNlNzdmM2IwNTdlOSIsImlzcyI6InVub2d3IiwiZXhwIjoxNzExNjY2ODk2LCJpYXQiOjE3MTE1ODA0OTYsImp0aSI6IlBTem8weFJOQVhFNlh5QTVPSmRrbVNKSVl3dVZVR2dTSGcybCJ9.ZrgGMgAr_oFiVbjSZoN7RdJR7xyPycxqyDQq9inL0AC8PBmrjW6qH3XYCBfKC39sYiy8azDlVvznrze54-sGNw'
 
   } catch {
 
