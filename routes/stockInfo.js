b435873b380a862c0adc6d8f8ab5bcd52e9886c0
Sieun Kim <se0983@kookmin.ac.kr>
2024-03-28 13:44:32 +0900

    feat: 소켓으로 받아오는 실시간 데이터에 시가, 고가, 저가 추가

--- routes/stockInfo.js
+++ routes/stockInfo.js
@@ -1,27 +1,28 @@
 var express = require("express");
 var infoRouter = express.Router();
 const { getStockInfo } = require("../utils/StockInfo");
-const { getAccessToken } = require("../utils/token/KOInvToken")
+const { getAccessToken } = require("../utils/token/KOInvToken");
 // 서버 실행시 access token 발급
-let accessToken = '';
+let accessToken = "";
 
 const getToken = async () => {
   try {
     // accessToken = await getAccessToken();
-    accessToken = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6IjFlZDMxY2YwLTExOTgtNDg0MC04MWI2LWIzM2VhOWIzZTIzYyIsImlzcyI6InVub2d3IiwiZXhwIjoxNzExNDk0MjQzLCJpYXQiOjE3MTE0MDc4NDMsImp0aSI6IlBTem8weFJOQVhFNlh5QTVPSmRrbVNKSVl3dVZVR2dTSGcybCJ9.XHV3KkfOe5TRgUgyFEbbtvLWItFE25jymRrUFUtkqaIwHL9tMpP-wxLJ9C29jiOqIRan3RAR8JN7qNuZCFqdxA'
-  } catch {
-
-  }
+    accessToken =
+      "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6ImVkZWEzNDE2LWYwYWQtNDY4MC04ODgwLTNlNzdmM2IwNTdlOSIsImlzcyI6InVub2d3IiwiZXhwIjoxNzExNjY2ODk2LCJpYXQiOjE3MTE1ODA0OTYsImp0aSI6IlBTem8weFJOQVhFNlh5QTVPSmRrbVNKSVl3dVZVR2dTSGcybCJ9.ZrgGMgAr_oFiVbjSZoN7RdJR7xyPycxqyDQq9inL0AC8PBmrjW6qH3XYCBfKC39sYiy8azDlVvznrze54-sGNw";
+  } catch {}
 };
 getToken();
 
-infoRouter.get('/:stockCode', async function (req, res, next) {
-    const stockCode = req.params.stockCode;
-    getStockInfo(stockCode, accessToken).then((infoObj) => {
-        res.json(infoObj);
-    }).catch((err) => {
-        console.log("DB Connection Failed:", err);
+infoRouter.get("/:stockCode", async function (req, res, next) {
+  const stockCode = req.params.stockCode;
+  getStockInfo(stockCode, accessToken)
+    .then((infoObj) => {
+      res.json(infoObj);
+    })
+    .catch((err) => {
+      console.log("DB Connection Failed:", err);
     });
 });
 
-module.exports = {infoRouter, accessToken};
+module.exports = { infoRouter, accessToken };
